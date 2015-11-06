70f9a6dacbcd107d9961baf7f0c084673024230b
Vladimir Pintea <vladimir.pintea@gmail.com>
2015-11-06 12:48:51 +0200

    Require base

--- lib/salt-parser.rb
+++ lib/salt-parser.rb
@@ -2,6 +2,7 @@ require "active_support"
 require "active_support/core_ext"
 
 require_relative "salt-parser/version"
+require_relative "salt-parser/base"
 require_relative "salt-parser/accounts"
 require_relative "salt-parser/errors"
 require_relative "salt-parser/builder"
